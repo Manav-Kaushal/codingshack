23a871d8c2d6d4d0227d9e16cfca07356a2bfe5f
Manav Kaushal <manavkaushal756@gmail.com>
2022-12-15 15:39:53 +0530

    setup app dir

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Welcome to CodingShacks Blog</h1>
+      <p>page</p>
+    </div>
+  );
+}
