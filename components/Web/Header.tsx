f6605d55f2a75a5bde3f1ebec5abf1e312fb8f48
Manav Kaushal <manavkaushal756@gmail.com>
2022-12-18 04:15:30 +0530

    frotend for blog list complete

--- components/Web/Header.tsx
+++ components/Web/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between space-x-2 h-[70px] shadow-md px-10">
+    <header className="flex items-center justify-between space-x-2 h-[70px] px-10">
       <div>
         <Link href="/">
           <Image
